bb55e524a179b02e8d68295f4c10c41bc47c45e6
motivation4you <66122479+motivation4you@users.noreply.github.com>
2020-06-06 21:56:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.swiper-container', {
+   loop: true,
+   pagination: {
+      el: '.swiper-pagination',
+   },
+});
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+   anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+
+      const blockID = anchor.getAttribute('href').substr(1)
+
+      document.getElementById(blockID).scrollIntoView({
+         behavior: 'smooth',
+         block: 'start'
+      })
+   })
+}
+
+new WOW().init();
